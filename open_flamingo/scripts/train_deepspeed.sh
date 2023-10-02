cb39b41363b614d0797aefd50d0b32536ace9311
Oscar Moll <orm@csail.mit.edu>
2023-10-02 11:48:19 -0400

    script for supercloud

--- open_flamingo/scripts/train_deepspeed.sh
+++ open_flamingo/scripts/train_deepspeed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=xeon-g6-volta
-#SBATCH --nodes=16
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=40
 #SBATCH --exclusive
@@ -11,7 +11,7 @@
 
 export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
-export HF_HOME=/home/gridsan/omoll/huggingface_cache/
+export HF_HOME=/home/gridsan/$USER/huggingface_cache/
 
 export PYTHONFAULTHANDLER=1
 export HOSTNAMES=`scontrol show hostnames "$SLURM_JOB_NODELIST"`
@@ -19,5 +19,8 @@ export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_PORT=15000
 export COUNT_NODE=`scontrol show hostnames "$SLURM_JOB_NODELIST" | wc -l`
 
+export WANDB_API_KEY=`cat /home/gridsan/$USER/.wandb_api_key`
+export WANDB_OFFLINE=1
+
 srun --output=%x_%j_%t.log --nodes=${SLURM_NNODES} --ntasks=${SLURM_NTASKS} --cpu-bind=cores --accel-bind=gv \
-    bash /home/gridsan/omoll/repos/open_flamingo/open_flamingo/scripts/run_node.bash
+    bash /home/gridsan/$USER/repos/open_flamingo/open_flamingo/scripts/run_node.bash
